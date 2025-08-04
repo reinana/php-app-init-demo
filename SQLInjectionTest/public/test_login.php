a1a09b4285b00c616c5cd99cf4e4b046b0745132
reinana <maronana30@gmail.com>
2025-08-04 16:11:39 +0900

    危険なクエリを修正

--- SQLInjectionTest/public/test_login.php
+++ SQLInjectionTest/public/test_login.php
@@ -14,9 +14,23 @@ if($charset === null) throw new Exception('Charset could be read');
 
 $username = $_POST['username'] ?? '';
 $password = $_POST['password'] ?? '';
+// 【危険なコード】ユーザー入力をそのままSQL文に埋め込む
+// $query = "SELECT * FROM test_users WHERE username = '$username' AND password = '$password';";
+// $result = $mysqli->query($query);
 
-$query = "SELECT * FROM test_users WHERE username = '$username' AND password = '$password';";
-$result = $mysqli->query($query);
+// 安全なコード
+// 1. SQL文の「骨格」を ? を使って準備する
+$stmt = $mysqli->prepare("SELECT * FROM test_users WHERE username = ? AND password = ?");
+
+// 2. ユーザー入力を「データ」として紐付ける ("ss"は2つとも文字列(string)の意味)
+$stmt->bind_param("ss", $username, $password);
+
+// 3. 実行する
+$stmt->execute();
+
+// 4. 結果を取得する
+$result = $stmt->get_result();
+
 $userData = $result->fetch_assoc();
 
 if ($userData) {
